e439fa83011f975245161aa8ab888fcb82cfe56a
javierreh <javiarroyoherrera@gmail.com>
2020-04-07 16:36:56 +0200

    Player movement and main, update and draw functions

--- js/game-project.js
+++ js/game-project.js
@@ -8,52 +8,94 @@ const lastX = startX + gameAreaSize;
 const startY = 0;
 const lastY = startY + gameAreaSize;
 
-let player = {
-    x: sizeElement * 7 + startX,
-    y: sizeElement * 7 + startY
+const player = {
+  x: sizeElement * 7 + startX,
+  y: sizeElement * 7 + startY,
+  size: 40,
+  move: 10
 }
 
-drawBackground();
-drawPlayer();
+let Keys = {
+  up: false,
+  down: false,
+  left: false,
+  right: false
+};
+
+setInterval(main, 100);
+
+function main() {
+  update();
+  draw();
+}
+
+function update() {
+  movePlayer();
+}
+
+function draw() {
+  drawBackground();
+  drawPlayer();
+}
 
 function drawBackground() {
-    // Draw Game Area
-    ctx.beginPath();
-    ctx.rect(startX, startY, gameAreaSize, gameAreaSize);
-    ctx.stroke();
+  // Clean background
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // Draw background border
-    // Top border
-    ctx.fillRect(startX, startY, sizeElement * 6, sizeElement * 1);
-    ctx.fillRect(lastX, startY, -sizeElement * 6, sizeElement * 1);
+  // Draw Game Area
+  ctx.fillStyle = "black";
+  ctx.beginPath();
+  ctx.rect(startX, startY, gameAreaSize, gameAreaSize);
+  ctx.stroke();
 
-    // Bottom border
-    ctx.fillRect(startX, lastY, sizeElement * 6, -sizeElement * 1);
-    ctx.fillRect(lastX, lastY, -sizeElement * 6, -sizeElement * 1);
+  // Draw background border
+  // Top border
+  ctx.fillRect(startX, startY, sizeElement * 6, sizeElement * 1);
+  ctx.fillRect(lastX, startY, -sizeElement * 6, sizeElement * 1);
 
-    // Left border
-    ctx.fillRect(startX, startY, sizeElement * 1, sizeElement * 6);
-    ctx.fillRect(startX, lastY, sizeElement * 1, -sizeElement * 6);
+  // Bottom border
+  ctx.fillRect(startX, lastY, sizeElement * 6, -sizeElement * 1);
+  ctx.fillRect(lastX, lastY, -sizeElement * 6, -sizeElement * 1);
 
-    // Right border
-    ctx.fillRect(lastX, startY, -sizeElement * 1, sizeElement * 6);
-    ctx.fillRect(lastX, lastY, -sizeElement * 1, -sizeElement * 6);
+  // Left border
+  ctx.fillRect(startX, startY, sizeElement * 1, sizeElement * 6);
+  ctx.fillRect(startX, lastY, sizeElement * 1, -sizeElement * 6);
+
+  // Right border
+  ctx.fillRect(lastX, startY, -sizeElement * 1, sizeElement * 6);
+  ctx.fillRect(lastX, lastY, -sizeElement * 1, -sizeElement * 6);
 }
 
 function drawPlayer() {
-    ctx.fillStyle = "darkgreen";
-    ctx.fillRect(player.x, player.y, sizeElement, sizeElement);
+  ctx.fillStyle = "darkgreen";
+  ctx.fillRect(player.x, player.y, player.size, player.size);
 }
 
-// Cuadrados de pruebas
-// ctx.fillStyle = "#FF0000";
-// ctx.fillRect(380, 0, 40, 40);
+window.onkeydown = function(event){
+  if (event.keyCode === 37) Keys.left = true;
+  if (event.keyCode === 38) Keys.up = true;
+  if (event.keyCode === 39) Keys.right = true;
+  if (event.keyCode === 40) Keys.down = true;
+};
 
-// ctx.fillStyle = "#FF0000";
-// ctx.fillRect(100, 280, 40, 40);
+window.onkeyup = function(event){
+  if (event.keyCode === 37) Keys.left = false;
+  if (event.keyCode === 38) Keys.up = false;
+  if (event.keyCode === 39) Keys.right = false;
+  if (event.keyCode === 40) Keys.down = false;
+};
 
-// ctx.fillStyle = "#FF0000";
-// ctx.fillRect(420, 600, -40, -40);
-
-// ctx.fillStyle = "#FF0000";
-// ctx.fillRect(700, 320, -40, -40);
+function movePlayer() {
+  if (Keys.up) {
+    player.y -= player.move;
+  }
+  if (Keys.down) {
+    player.y += player.move;
+  }
+  if (Keys.left) {
+    player.x -= player.move;
+  }
+  if (Keys.right) {
+    player.x += player.move;
+  }
+}
